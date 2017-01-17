e5b3a44403b96a4e0deead0ea420bee4dca85638
andriyka <andriyka@playtech.com>
2017-01-17 06:46:34 +0000

    [ISSUE #12]

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-	openssl aes-256-cbc -K $encrypted_1fc90f464345_key -iv $encrypted_1fc90f464345_iv -in codesigning.asc.enc -out codesigning.asc -d
+	openssl aes-256-cbc -K $encrypted_1fc90f464345_key -iv $encrypted_1fc90f464345_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+	gpg --fast-import cd/codesigning.asc
 fi
