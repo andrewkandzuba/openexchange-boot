b068c507b9080e3db1a2305d45160516d40a499d
andriyka <andriyka@playtech.com>
2017-01-17 06:28:20 +0000

    [ISSUE #15]

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 	openssl aes-256-cbc -K $encrypted_1fc90f464345_key -iv $encrypted_1fc90f464345_iv -in codesigning.asc.enc -out codesigning.asc -d
--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn deploy -P sign,build-extras --settings cd/mvnsettings.xml
