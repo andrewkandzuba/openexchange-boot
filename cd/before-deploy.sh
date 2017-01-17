0d5e627afe5467e057126dda7958a35029a9c07f
andriyka <andriyka@playtech.com>
2017-01-17 06:30:50 +0000

    [ISSUE #12]

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 	openssl aes-256-cbc -K $encrypted_1fc90f464345_key -iv $encrypted_1fc90f464345_iv -in codesigning.asc.enc -out codesigning.asc -d
